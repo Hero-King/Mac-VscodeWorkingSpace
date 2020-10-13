f16a672e09bc33b590b540b41872414d698a4c4d
Hero-King <838774057@qq.com>
2020-10-13 23:46:46 +0800

    add

--- antdesign-stu/src/reactImage.jsx
+++ antdesign-stu/src/reactImage.jsx
@@ -5,7 +5,7 @@ export default function(){
     return (
         <div id="react-image">
             {/* react中引入图片是不可以这么直接引入的 需要使用模块化导入 */}
-            <img src="../public/images/logo512.png" alt="error"/>   
+            {/* <img src="../public/images/logo512.png" alt="error"/>    */}
 
             <img src={logo} alt="module error" srcset=""/>
 
