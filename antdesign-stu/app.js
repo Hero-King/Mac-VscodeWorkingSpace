49acb8f5ef2bb030e5a495de04d92775860d7e34
Hero-King-Linux <838774057@qq.com>
2020-11-17 22:27:44 +0800

    添加antd官方api部署脚本

---
+++ antdesign-stu/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+let antdesignstuPath = "/home/git/Mac-VscodeWorkingSpace/antdesign-stu"
+app.use(express.static(antdesignstuPath + '/build'));
+app.get('/index-cn', function (req, res) {
+	console.log(req.query,'response index-cn.html')
+    res.sendFile(path.join(antdesignstuPath, 'build', 'index-cn.html'));
+});
+
+app.get('/*',function(req, res){
+     console.log('express have not route')   
+res.end('express have not route')
+});
+
+app.listen(80);
