b67708eb85bf2dafa2e8328e1e71f6228c313260
wangjj <centos.com>
2021-03-30 23:02:16 +0800

    学习reacthooks

--- antdesign-stu/src/reactApi.js
+++ antdesign-stu/src/reactApi.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useContext } from 'react';
 // 学习使用react hook
 
 let res = []
@@ -13,6 +13,17 @@ for (let index = 0; index < 10; index++) {
 
 // 我该如何实现 shouldComponentUpdate?
 // 你可以用 React.memo 包裹一个组件来对它的 props 进行浅比较：
+
+// context 上下文  可以理解成全局变量 创建出来的变量一般写在模块中暴露出来
+const MyContext = React.createContext("default");
+/* 
+API : 
+<MyContext.Provider value={***}>
+<MyContext.Consumer>
+  {value => // 基于 context 值进行渲染}
+</MyContext.Consumer>
+
+*/
 
 function ReactApi(props) {
 
@@ -45,8 +56,40 @@ function ReactApi(props) {
                 )
             }
         </div>
+        <TestContext/>
     </div>
 }
+
+function TestContext(props) {
+    return <div>
+        <MyContext.Provider value="newValue">
+            <MyDiv/>
+            <MyDivHooks/>
+        </MyContext.Provider>
+        <MyDiv/>
+    </div>
+}
+
+
+function MyDiv(props) {
+    return <MyContext.Consumer>   
+        {value => <div>{value}</div>}
+    </MyContext.Consumer>
+}
+
+function MyDivHooks(props) {
+    const value = useContext(MyContext)
+    return <div>
+        Hooks获取context数据： {value}
+    </div>
+}
+
+/**
+ * 如果你在接触 Hook 前已经对 context API 比较熟悉，那应该可以理解，useContext(MyContext) 相当
+ * 于 class 组件中的 static contextType = MyContext 或者 <MyContext.Consumer>。
+useContext(MyContext) 只是让你能够读取 context 的值以及订阅 context 的变化。你仍然需要在上层组件树中使用 
+<MyContext.Provider> 来为下层组件提供 context。
+ */
 
 
 
