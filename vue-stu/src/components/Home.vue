4d6d2aebc67905a97cf25192a80ea80e2a8cae32
Hero-King-yun <838774057@qq.com>
2021-08-16 22:11:24 +0800

    study vuedraggable & 项目配置支持less

--- vue-stu/src/components/Home.vue
+++ vue-stu/src/components/Home.vue
@@ -16,10 +16,9 @@
         </el-submenu>
         <el-submenu index="3">
           <template slot="title"><i class="el-icon-setting"></i>工具组件学习</template>
-            <el-menu-item index="draggable">draggable</el-menu-item>
+            <el-menu-item index="/home/draggable">draggable</el-menu-item>
             <el-menu-item index="3-2">选项2</el-menu-item>
             <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-menu-item-group>
           <el-submenu index="3-4">
             <template slot="title">选项4</template>
             <el-menu-item index="3-4-1">选项4-1</el-menu-item>
--- vue-stu/src/components/draggable-stu.vue
+++ vue-stu/src/components/draggable-stu.vue
@@ -1,10 +1,56 @@
 <template>
   <div>
     <h2>draggable</h2>
-    <draggable v-model="myArray" group="people" @start="drag = true" @end="drag = false">
-      <div v-for="element in myArray" :key="element.id">{{ element.name }}</div>
-      <button slot="header" @click="i++">Add</button>
-    </draggable>
+    <div class="module">
+      <draggable v-model="myArray" @start="drag = true" @end="drag = false">
+        <div v-for="element in myArray" :key="element.id">{{ element.name }}</div>
+        <button slot="header" @click="handleAdd">Add</button>
+      </draggable>
+    </div>
+
+    <h2>delay属性</h2>
+    <div class="module">
+      <draggable v-model="myArray" delay="100">
+        <div v-for="element in myArray" :key="element.id">{{ element.name }}</div>
+        <button slot="header" @click="handleAdd">Add</button>
+      </draggable>
+    </div>
+
+    <h2>disabled属性</h2>
+    <div class="module">
+      <draggable v-model="myArray" :disabled="true">
+        <div v-for="element in myArray" :key="element.id">{{ element.name }}</div>
+        <button slot="header" @click="handleAdd">Add</button>
+      </draggable>
+    </div>
+
+    <h2>animation属性</h2>
+    <p>拖动时的动画效果，还是很酷的,数字类型。如设置animation=1000表示1秒过渡动画效果</p>
+    <div class="module">
+      <draggable v-model="myArray" :animation="1000">
+        <div v-for="element in myArray" :key="element.id">{{ element.name }}</div>
+        <button slot="header" @click="handleAdd">Add</button>
+      </draggable>
+    </div>
+
+    <h2>handle</h2>
+    <p>只有当鼠标移动到css为mover类的元素上才能拖动</p>
+    <div class="module">
+      <draggable v-model="myArray" tag="p" handle=".mover">
+        <div class="mover" v-for="element in myArray" :key="element.id">{{ element.name }}</div>
+        <button slot="header" @click="handleAdd">Add</button>
+      </draggable>
+    </div>
+
+    <h2>chosen-class 拖拽时候的样式</h2>
+    <div class="module">
+      <draggable v-model="myArray" tag="p" chosen-class="chosen" :force-fallback="true">
+        <div class="mover" v-for="element in myArray" :key="element.id">{{ element.name }}</div>
+        <button slot="header" @click="handleAdd">Add</button>
+      </draggable>
+    </div>
+
+
   </div>
 </template>
 
@@ -14,6 +60,12 @@ export default {
   name: 'DraggableStu',
   components: {
     draggable
+  },
+  methods: {
+    handleAdd(e) {
+      this.i++
+      console.log(this.i)
+    }
   },
   data() {
     return {
@@ -36,3 +88,17 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+.module {
+  p {
+    > div {
+      padding: 3px;
+    }
+  }
+
+  .chosen{
+    border: 2px solid blue;
+  }
+}
+</style>
