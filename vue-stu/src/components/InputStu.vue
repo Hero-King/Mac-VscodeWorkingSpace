ee03b796d66162ddd494fba89a0cd263c523ca2a
book-pc <838774057@qq.com>
2023-03-26 22:58:04 +0800

    element input

--- vue-stu/src/components/InputStu.vue
+++ vue-stu/src/components/InputStu.vue
@@ -2,20 +2,26 @@
   <div class="wrap" title="">
     <h3>使用v-model绑定,数据为字符串类型</h3>
     <div class="item">
-      <el-input placeholder="请输入内容" v-model="input"> </el-input>
+      <el-input v-model="input" placeholder="请输入内容" />
       <span>value: {{ input }} typeof value : {{ typeof input }}</span>
     </div>
 
     <h3>v-model.number 可以输入英文字符; 输入英文字符时类型是String, 输入数字时候是Number类型, 不好用</h3>
     <div class="item">
-      <el-input placeholder="请输入内容" v-model.number="input1"> </el-input>
+      <el-input v-model.number="input1" placeholder="请输入内容" />
       <span>value: {{ input1 }} typeof value : {{ typeof input1 }}</span>
     </div>
 
     <h3>NumberInput</h3>
     <div class="item">
-      <NumberInput placeholder="请输入内容" :decimal="5" v-model="numberInput"> </NumberInput>
+      <NumberInput v-model="numberInput" placeholder="请输入内容" :decimal="5" />
       <span>value: {{ numberInput }} typeof value : {{ typeof numberInput }}</span>
+    </div>
+
+    <h3>label属性</h3>
+    <div class="item">
+      <el-button id="prettier-id" class-name="prettier-class" type="primary" circle> Click Here </el-button>
+      <el-input label="姓名" />
     </div>
   </div>
 </template>
