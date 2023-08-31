2ded9b1f002a545dded5f1dfbfe3fb4051639fad
book-pc <838774057@qq.com>
2023-08-31 23:25:49 +0800

    test vite

--- vue3-ts-stu/src/main.ts
+++ vue3-ts-stu/src/main.ts
@@ -7,6 +7,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// test
+import style from './components/test.css?inline'
+import moduleStyle from './components/style.module.css'
+// style 是string类型的变量, 
+console.log(typeof style,'style', "------", "moduleStyleExport", typeof moduleStyle, moduleStyle.module);  // 前面是string  后面moduleStyle 就是object
+
+
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(createPinia())
