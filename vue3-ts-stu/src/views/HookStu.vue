3e1bd20a5bb1cd9c443e2cfb9311658058f48c81
wangjunjie <SV00249433@Svolt.cn>
2023-10-18 08:10:18 +0800

    vue-hooks stu

--- vue3-ts-stu/src/views/HookStu.vue
+++ vue3-ts-stu/src/views/HookStu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div ref="fullscreen">
     hook Stu
     <div>
       计数器 {{ count }}
@@ -20,6 +20,29 @@
     </div>
 
     <div class="module">useVModel 解决父组件传递对象到子组件, 子组件中使用v-model绑定问题</div>
+
+    <div class="module">
+      useEventListener 轻松使用事件监听。在挂载时使用 addEventListener 注册，在卸载时自动使用
+      removeEventListener 。
+    </div>
+
+    <div class="module">useFullscreen 全屏模式呈现特定元素（及其后代）的方法</div>
+    <div class="module" @click="showSidebar = !showSidebar">
+      useElementSize 获取元素尺寸大小响应式 ; useResizeObserver 监听元素内容区和边框尺寸的变化。
+      <a
+        href="https://developer.mozilla.org/zh-CN/docs/Web/API/ResizeObserver"
+        target="_blank"
+        rel="noreferrer"
+        >ResizeObserver MDN</a
+      >
+      <div class="flex">
+        <div v-show="showSidebar" class="flex-1 border">sidebar</div>
+        <div ref="content" class="flex-1 border">
+          content width, height: {{ width }}, {{ height }}
+        </div>
+      </div>
+    </div>
+    <div class="module">useMutationObserver 监听元素样式,大小,子元素变化</div>
   </div>
 </template>
 
@@ -30,22 +53,41 @@ import {
   usePreferredDark,
   useEventListener,
   onClickOutside,
-  useStorage
+  useStorage,
+  useFullscreen,
+  useElementSize,
+  useResizeObserver
 } from '@vueuse/core'
 import { useTest } from '@/hook/test'
 const { x, y } = useMouse()
+const fullscreen = ref<HTMLElement>()
 const count = ref(0)
 const container = ref()
+const content = ref()
 const inputVal = useStorage('inputVal', 'defaultVal')
+const showSidebar = ref<boolean>(true)
 
 // vueuse 中 副作用会在组件卸载时候自动清除, 你也可以手动接收hook返回值作为资源清理函数
-useEventListener('click', () => {
-  console.log('handle click')
+useEventListener(window, 'resize', (e) => {
+  console.log('handle resize', e)
 })
+const { isSupported, isFullscreen, enter, exit, toggle } = useFullscreen(fullscreen)
 // 不给hook传递参数 hook内部逻辑只会执行一次
 // const { data } = useTest()
 
 const { data } = useTest(count)
+
+const { width, height } = useElementSize(content)
+console.log('width, height', width, height)
+useResizeObserver(content, (entries) => {
+  console.log(entries, 'entries')
+  const entry = entries[0]
+  const { width, height } = entry.contentRect
+})
+
+onMounted(() => {
+  console.log('isFullscreen', isFullscreen, 'isSupported', isSupported)
+})
 
 onClickOutside(container, () => alert('onClickOutside callback'))
 
