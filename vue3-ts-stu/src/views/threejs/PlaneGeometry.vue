fcf26cbe6f6f5d9393ffc49227e5a28515731d77
book-pc <838774057@qq.com>
2023-12-22 23:35:10 +0800

    threejs side

--- vue3-ts-stu/src/views/threejs/PlaneGeometry.vue
+++ vue3-ts-stu/src/views/threejs/PlaneGeometry.vue
@@ -18,12 +18,13 @@ onMounted(() => {
   // 设置旋转的原点 纹理的旋转点范围是0~1 默认是(0,0) 即左下角
   // doorColorTexture.center.set(0.5, 0.5);
 
-  // 创建5*5的平面
+  // 创建5*5的矩形平面
   const geometry = new THREE.PlaneGeometry(5, 5)
-  // 创建带纹理的材质
+  // 创建带纹理的材质, threejs材质默认正面可见，反面不可见 需要设置side属性使得双面可见
   const material = new THREE.MeshBasicMaterial({
     color: 0xffff00,
-    map: doorColorTexture
+    map: doorColorTexture,
+    side: THREE.DoubleSide
   })
   // 根据几何体和材质创建物体
   const mesh = new THREE.Mesh(geometry, material)
