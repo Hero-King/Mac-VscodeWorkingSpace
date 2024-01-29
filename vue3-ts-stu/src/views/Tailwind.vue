392b23525e19b6d7486c8520742e008b7a14eb39
wangjunjie <SV00249433@Svolt.cn>
2024-01-29 15:18:55 +0800

    tailwind css

--- vue3-ts-stu/src/views/Tailwind.vue
+++ vue3-ts-stu/src/views/Tailwind.vue
@@ -3,7 +3,7 @@
   <div class="flex items-center bg-gray-200 font-semibold text-white">flex布局</div>
   <div class="relative">定位</div>
   <div class="px-1 py-5 mx-auto">padding/margin</div>
-  <div class="bg-white rounded text-center">背景色</div>
+  <div class="bg-white rounded text-center">背景色 圆角 文本对齐</div>
   <span class="w-full">宽高</span>
   <div class="columns-2">
     <span>columns</span>
